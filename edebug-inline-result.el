2c8ba9a98efc100c55ee7e827b192e90c48073fb
stardiviner <numbchild@gmail.com>
2019-03-04 13:46:22 +0800

    add more backends: quick-peek, inline-docs

--- edebug-inline-result.el
+++ edebug-inline-result.el
@@ -13,18 +13,19 @@
   (interactive)
   (cond
    ((featurep 'posframe)
-    (posframe-show
-     " *edebug-previous-result*"
-     :string (propertize edebug-previous-result) ; FIXME: did not auto wrap and fill.
-     :position (point)
-     :background-color (cl-case (alist-get 'background-mode (frame-parameters))
-                         ('light
-                          (color-darken-name (face-background 'default) 10))
-                         ('dark
-                          (color-lighten-name (face-background 'default) 15)))))
+    (posframe-show " *edebug-previous-result*"
+                   :string (substring-no-properties edebug-previous-result)
+                   :position (point)
+                   :background-color "DarkCyan"
+                   :foreground-color "white"
+                   :width 50))
    ((featurep 'popup)
     (popup-tip edebug-previous-result
                :truncate t :height 20 :width 45 :nostrip t :margin 1 :nowait nil))
+   ((featurep 'quick-peek)
+    (quick-peek-show edebug-previous-result))
+   ((featurep 'inline-docs)
+    (inline-docs edebug-previous-result))
    ((featurep 'pos-tip)
     (pos-tip-show edebug-previous-result 'popup-face))))
 
