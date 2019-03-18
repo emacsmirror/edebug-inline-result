5fac953e1e9ab97e8d3b979b647f40d6a9b10972
stardiviner <numbchild@gmail.com>
2019-03-18 09:56:36 +0800

    add package metadata headers

--- edebug-inline-result.el
+++ edebug-inline-result.el
@@ -1,10 +1,30 @@
 ;;; edebug-inline-result.el --- show Edebug result inline.
 
-;;; Time-stamp: <2019-03-09 12:28:59 stardiviner>
+;; Authors: stardiviner <numbchild@gmail.com>
+;; Package-Requires: ((emacs "25") (cl-lib "0.5"))
+;; Package-Version: 0.1
+;; Keywords: edebug inline
+;; homepage: https://www.github.com/stardivin
+
+;; edebug-inline-result is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; edebug-inline-result is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Time-stamp: <2019-03-14 14:31:24 stardiviner>
 
 ;;; Commentary:
 
-
+;;; TODO: reference `lsp-ui-doc'
 
 ;;; Code:
 
